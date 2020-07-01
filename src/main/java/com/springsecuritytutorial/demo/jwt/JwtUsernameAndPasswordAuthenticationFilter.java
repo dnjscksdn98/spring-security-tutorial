aa834cf9537c617a2845a8810d3aa307b1c7a168
Alex <dnjscksdn98@gmail.com>
2020-07-01 22:09:53 +0900

    successfulAuthentication() 오버라이딩

--- src/main/java/com/springsecuritytutorial/demo/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
+++ src/main/java/com/springsecuritytutorial/demo/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -1,18 +1,24 @@
 package com.springsecuritytutorial.demo.jwt;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Date;
 
 /**
- * Verify the credentials
+ * Request Filter
  */
 public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -68,5 +74,37 @@ public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePassword
         } catch(IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    /**
+     * attemptAuthentication 실행 후에 성공시 successfulAuthentication 호출
+     * 토큰 생성후 응답(Response) 헤더(Header)에 추가한 후에 클라이언트로 전송
+     * 
+     * @param request
+     * @param response
+     * @param chain
+     * @param authResult
+     * @throws IOException
+     * @throws ServletException
+     */
+    @Override
+    protected void successfulAuthentication(
+            HttpServletRequest request, 
+            HttpServletResponse response, 
+            FilterChain chain, 
+            Authentication authResult
+    ) throws IOException, ServletException {
+
+        String key = "securesecuresecuresecuresecuresecuresecure";
+
+        String token = Jwts.builder()
+                .setSubject(authResult.getName())
+                .claim("authorities", authResult.getAuthorities())
+                .setIssuedAt(new Date())
+                .setExpiration(java.sql.Date.valueOf(LocalDate.now().plusWeeks(2)))
+                .signWith(Keys.hmacShaKeyFor(key.getBytes()))
+                .compact();
+
+        response.addHeader("Authorization", "Bearer " + token);
     }
 }
