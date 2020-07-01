ba9cba7ed847872582bb92a96e4476d5065f6246
Alex <dnjscksdn98@gmail.com>
2020-07-01 22:11:06 +0900

    Filter 및 sessionManagement 설정 추가

--- src/main/java/com/springsecuritytutorial/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/springsecuritytutorial/demo/security/ApplicationSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.springsecuritytutorial.demo.security;
 
 import com.springsecuritytutorial.demo.auth.ApplicationUserService;
+import com.springsecuritytutorial.demo.jwt.JwtUsernameAndPasswordAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static com.springsecuritytutorial.demo.security.ApplicationUserRole.STUDENT;
@@ -35,13 +37,14 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager()))
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
                 .anyRequest()
-                .authenticated()
-                .and()
-                .formLogin();
+                .authenticated();
     }
 
     @Override
